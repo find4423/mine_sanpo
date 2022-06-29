8e646f82a3d90ce3bcd1ff98ddfa190166ac9583
find4423 <106136699+find4423@users.noreply.github.com>
2022-06-29 17:46:49 +0900

    削除機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :post_destroy, only: :destroy
+  
   def index
     @posts = Post.all
   end
@@ -16,9 +18,22 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    post = Post.find(params[:id])
+    if post.destroy
+      redirect_to root_path
+    end
+  end
+
   private
 
   def post_params
     params.require(:post).permit(:text, :image).merge(user_id: current_user.id)
+  end
+
+  def post_destroy
+    if user_signed_in? && current_user.id == post.user_id
+    end
+    redirect_to root_path
   end
 end
